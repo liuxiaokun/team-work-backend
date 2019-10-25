78f0f8768f44e113edb8bf07ae3582f674d40a6c
liuxiaokun <liuxiaokun@baiyixinkeji.com>
2019-10-25 16:56:13 +0800

    first init

---
+++ src/main/java/com/byx/work/work/team/WorkTeamApplication.java
@@ -0,0 +1,13 @@
+package com.byx.work.work.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkTeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkTeamApplication.class, args);
+    }
+
+}
